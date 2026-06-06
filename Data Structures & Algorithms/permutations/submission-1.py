3f9a9382c8d5c38e7ac015b17ed1bcc5225d0e8d
madhukar-vissapragada <95085208+madhukar-vissapragada@users.noreply.github.com>
2026-06-06 12:45:59 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        self.solve(nums, 0, result)
+        return result  
+    
+    def solve(self, nums: List[int], current_index: int, result: List[List[int]]):
+        if current_index == len(nums):
+            result.append(nums[:])
+            return 
+        
+
+        for index in range(current_index, len(nums)):
+            nums[index], nums[current_index] = nums[current_index], nums[index]
+            self.solve(nums, current_index + 1, result)
+            nums[index], nums[current_index] = nums[current_index], nums[index]
+        
+        
