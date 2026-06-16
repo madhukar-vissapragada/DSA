20b56e1e0bdcdacf3cb075743c7bf9b03f104c02
madhukar-vissapragada <95085208+madhukar-vissapragada@users.noreply.github.com>
2026-06-16 13:33:06 +0530

    Add: permutations-ii - submission-2

---
+++ Data Structures & Algorithms/permutations-ii/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        self.solve(nums, 0, result)
+        return list(result)
+
+    def solve(self, nums: List[int], current_index: int, result: List[List[int]]):
+        if current_index == len(nums):
+            result.append(nums.copy())
+            return
+
+        seen = set()
+        for index in range(current_index, len(nums)):
+            if nums[index] in seen:
+                continue 
+            seen.add(nums[index])
+            nums[index], nums[current_index] = nums[current_index], nums[index]
+            self.solve(nums, current_index + 1, result)
+            nums[current_index], nums[index] = nums[index], nums[current_index]
