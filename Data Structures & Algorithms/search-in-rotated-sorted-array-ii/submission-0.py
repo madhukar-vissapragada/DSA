2e3fe040370a146ff3cca9b698ad4fa948b4c72b
madhukar-vissapragada <95085208+madhukar-vissapragada@users.noreply.github.com>
2026-07-06 17:08:38 +0530

    Add: search-in-rotated-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-0.py
@@ -0,0 +1,29 @@
+from typing import *
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return True
+
+            if nums[mid] > target:
+                if nums[mid] >= low and nums[mid] < target:
+                    high = low - 1
+                else:
+                    low = high + 1
+            else:
+                if nums[mid] < target and nums[high] >= target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+                    
+        for current in nums:
+            if current == target:
+                return True
+
+        return False
