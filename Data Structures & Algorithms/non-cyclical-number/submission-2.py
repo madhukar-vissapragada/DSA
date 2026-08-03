d2ee148a8e501f444a72ceb022d3d2c0a114e067
madhukar-vissapragada <95085208+madhukar-vissapragada@users.noreply.github.com>
2026-08-03 18:11:03 +0530

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        return self.solve(n, set())
+
+    def solve(self, n: int, hash_set: set) -> bool:
+        if n == 1:
+            return True
+
+
+        sum_ = 0
+        while n > 0:
+            sum_ += (n % 10) ** 2
+            n = n // 10
+
+        if sum_ in hash_set:
+            return False
+
+        hash_set.add(sum_)
+        return self.solve(sum_, hash_set)
